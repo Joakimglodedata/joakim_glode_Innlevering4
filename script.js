10a94a6ab70461f96421d6569e70d2a5c2a06e1a
Joakimglodedata <joakim.glodedata@gmail.com>
2025-04-29 14:18:05 +0200

    Mange linker, mange knapper

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+document.querySelector("#aktivknapp").onclick = function() {var x = document.querySelector("#linker")
+    if (x.style.display === "block") {
+        x.style.display = "none"
+      } else {
+        x.style.display = "block"
+      }
+      var y = document.querySelector("#dødknapp")
+      if (y.style.display === "block") {
+          y.style.display = "none"
+        } else {
+          y.style.display = "block"
+    } }
