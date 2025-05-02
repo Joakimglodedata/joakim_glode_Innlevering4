a3300c47da3ec8ee91da1ec1b18612c0c878397a
Joakimglodedata <joakim.glodedata@gmail.com>
2025-05-02 13:54:38 +0200

    endra

--- script.js
+++ script.js
@@ -24,11 +24,36 @@ aktivk.onclick = burgerfunksjon
 dødk.onclick = burgerfunksjon
 overlaym.onclick = burgerfunksjon
 
-function burgerfunksjon() {if (linkerl.style.display = "block") {aktivk.style.display = "none"
-dødk.style.display = "block"
-linkerl.style.display = "block"
-overlaym.style.display = "block"} else {dødk.style.display = "none"
-aktivk.style.display = "block"
-linkerl.style.display = "none"
-overlaym.style.display = "none"}
+let keyDown = {'kva som helst': false}
+
+document.addEventListener("keydown", keyboardpress)
+function keyboardpress(e) {
+  e.preventDefault()
+  keyDown[e.key] = true
+  if (keyDown["m" || "Esc"]) {
+    burgerfunksjon()
+  }
+}
+
+function burgerfunksjon() {
+    var aktivk = document.querySelector("#aktivknapp")
+    var dødk = document.querySelector("#dødknapp")
+    var linkerl = document.querySelector("#linker")
+    var overlaym = document.querySelector("#overlay")
+
+    if (linkerl.style.display === "none" && window.innerWidth < 800) {
+        aktivk.style.display = "none"
+        dødk.style.display = "block"
+        linkerl.style.display = "block"
+        overlaym.style.display = "block"
+    } else if (window.innerWidth < 800) {
+        aktivk.style.display = "block"
+        dødk.style.display = "none"
+        linkerl.style.display = "none"
+        overlaym.style.display = "none"
+    } else {linkerl.style.display = "flex"
+        aktivk.style.display = "none"
+        dødk.style.display = "none"
+        overlaym.style.display = "none"
+}
 }
